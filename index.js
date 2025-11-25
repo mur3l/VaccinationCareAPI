ecc5c04fb1db183bb030176830d5010fc24f4783
JelenaPenkina <122533981+JelenaPenkina@users.noreply.github.com>
2025-11-25 15:46:26 +0200

    Update Swagger
    .yaml file. Võtan siit andmed https://studio.apicur.io/apis/122496/editor "GET" abil

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 require('dotenv').config();
 const port = process.env.PORT || 8080;
 const host = 'localhost';
-const express = require('cors');
+const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const swaggerUI = require('swagger-ui-express');
 const yamljs = require('yamljs');
 
-const swaggerDocument = yamljs.load('./docs/swagger.yaml');
+const swaggerDocument = require('./docs/swagger.json');
+//const swaggerDocument = yamljs.load('./docs/swagger.yaml');
 
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
