fce2cc4d6431dc27cc61cdea12a26fff7e2c3f3c
mur3l <tippitauno@gmail.com>
2025-11-25 14:23:05 +0200

    added project files. initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const port = process.env.PORT || 8080;
+const host = 'localhost';
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const swaggerUI = require('swagger-ui-express');
+const yamljs = require('yamljs');
+
+//const swaggerDocument = yamljs.load('./docs/swagger.json');
+const swaggerDocument = require('./docs/swagger.json')
+
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+
+app.listen(port, () => {
+    console.log(`API on addressil: http://localhost:${port}`)
+})
+
