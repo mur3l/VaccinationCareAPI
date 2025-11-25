d492b2778ed6aba808fe0a382f845541add570a1
JelenaPenkina <122533981+JelenaPenkina@users.noreply.github.com>
2025-11-25 14:18:22 +0200

    Index.js Update

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const port = process.env.PORT || 8080;
+const host = 'localhost';
+const express = require('cors');
+const app = express();
+
+const swaggerUI = require('swagger-ui-express');
+const yamljs = require('yamljs');
+
+const swaggerDocument = yamljs.load('./docs/swagger.yaml');
+
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+app.listen(port, () => {
+    console.log(`API on aadressil: http://localhost:${port}`);
+})
