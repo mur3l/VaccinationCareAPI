f77d720b9d2c539580a6b6de150649f46491e33f
mur3l <tippitauno@gmail.com>
2025-11-25 14:39:10 +0200

    Fix COMMIT

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const app = express();
 const swaggerUI = require('swagger-ui-express');
 const yamljs = require('yamljs');
 
-const swaggerDocument = yamljs.load('./docs/swagger.yaml');
+//const swaggerDocument = yamljs.load('./docs/swagger.yaml');
+const swaggerDocument = require('./docs/swagger.json');
 
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
