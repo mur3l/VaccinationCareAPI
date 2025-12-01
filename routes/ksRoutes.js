70c5597f19de8ca094dd1b08e455554d4ebe14df
mur3l <tippitauno@gmail.com>
2025-12-01 17:40:28 +0200

    Test Vacc ja muu pudi padi

--- routes/ksRoutes.js
+++ routes/ksRoutes.js
@@ -2,4 +2,7 @@ const VaccinationController = require("../controllers/VaccinationController")
 
 module.exports = (app) => {
     app.route("/vaccinations")
+    .get(VaccinationController.getAll)
+    app.route("/vaccinations/:VaccineID")
+    .get(VaccinationController.getByID)
 }
