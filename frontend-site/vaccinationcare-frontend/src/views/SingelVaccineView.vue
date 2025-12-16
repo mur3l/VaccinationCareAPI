99b0dce3468781b6c159b3be064756bde569934a
mur3l <tippitauno@gmail.com>
2025-12-16 15:17:02 +0200

    438807 - As a User I can view the details of a Vaccine, in my browser.

---
+++ frontend-site/vaccinationcare-frontend/src/views/SingelVaccineView.vue
@@ -0,0 +1,72 @@
+<script>
+    import VaccineDetailsTable from `../components/VaccineDetailsTable`;
+    export default{
+        components: {
+                    thisVaccine: {
+                    VaccineID: "",
+                    Name: "",
+                    Description: "des",
+                    Clinic: "",
+                    Appointment: "16.12.2025",
+                    Location: "",
+                    BestBefore: 2025
+                }
+            },
+        props: {
+            seekID:{
+                type: String,
+                required: true
+            },
+
+        data() {
+            return {
+                thisVaccine: {
+                    VaccineID: "",
+                    Name: "",
+                    Description: "des",
+                    Clinic: "",
+                    Appointment: "16.12.2025",
+                    Location: "",
+                    BestBefore: 2025
+                }
+            }
+        }
+    },
+        async created() {
+            this.thisVaccine = await (await fetch(`http://
+            localhost:8080/vaccinations/${seekID}`)).json();
+        }
+    }
+</script>
+<template>
+    <table class="table table-striped">
+        <tr>
+            <td>Vaccine ID</td>
+            <td>{{thisVaccine.VaccineID}}</td>
+        </tr>
+        <tr>
+            <td>Nimi</td>
+            <td>{{thisVaccine.Name}}</td>
+        </tr>
+        <tr>
+            <td>Description</td>
+            <td>{{thisVaccine.Description}}</td>
+        </tr>
+        <tr>
+            <td>Clinic ID</td>
+            <td>{{thisVaccine.Clinic}}</td>
+        </tr>
+        <tr>
+            <td>Appointment</td>
+            <td>{{thisVaccine.Appointment}}</td>
+        </tr>
+        <tr>
+            <td>Location</td>
+            <td>{{thisVaccine.Location}}</td>
+        </tr>
+        <tr>
+            <td>BestBefore</td>
+            <td>{{thisVaccine.BestBefore}}</td>
+        </tr>
+    </table>
+</template>
