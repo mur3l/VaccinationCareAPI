a66281672a0b78c5beb77334ee18d5a07dbdef1f
JelenaPenkina <122533981+JelenaPenkina@users.noreply.github.com>
2026-01-10 20:29:23 +0200

    438833 As an administrator I want to be able to remove a vaccine from my browser
    As an administrator I want to be able to remove a vaccine from my browser

--- frontend-site/vaccinationcare-frontend/src/components/VaccineTable.vue
+++ frontend-site/vaccinationcare-frontend/src/components/VaccineTable.vue
@@ -5,13 +5,12 @@
             items: Array
         },
         methods: {
-            async deleteVaccine (vaccineID) {
-                this.result = await (await fetch(`http://localhost:8080/vaccination/${this.VaccineID}`, {method: 'DELETE'}))
+            async deleteVaccine(vaccineID) {
+                await fetch(`http://localhost:8080/vaccination/${vaccineID}`, { method: 'DELETE' });
             },
-                modifyVaccine(vaccineID) {
-            this.$router.push({ name: 'modifyVaccine', params: { seekID: vaccineID } });
-        }
-
+            modifyVaccine(vaccineID) {
+                this.$router.push({ name: 'modifyVaccine', params: { seekID: vaccineID } });
+            }
         }
     }
 </script>
@@ -22,27 +21,54 @@
             <tr>
                 <th>Vaccine ID</th>
                 <th>Vaccine Name</th>
+                <th>Actions</th>
             </tr>
         </thead>
-            <tbody>
-                <tr v-for="item in items" :key="item.VaccineID">
-                    <td>{{ item.VaccineID }}</td>
-                    <td>{{item.Name}}</td>
-                    <td><router-link :to="{name:'vaccine', params: {seekID: item.VaccineID}}">
-                        <button @click="navigate">ä</button>
+        <tbody>
+            <tr v-for="item in items" :key="item.VaccineID">
+                <td>{{ item.VaccineID }}</td>
+                <td>{{item.Name}}</td>
+                <td class="actions">
+                    <router-link :to="{ name: 'vaccine', params: { seekID: item.VaccineID } }">
+                    <button class="action-btn details">View</button>
                     </router-link>
-                    </td>
-                    <td>
-                        <router-link :to="{name:'vaccine'}">
-                            <button @click="deleteVaccine(item.VaccineID)"></button>
-                        </router-link>
-                    </td>
-                    <td>
-                        <button @click="modifyVaccine(item.VaccineID)">Modify</button>
-                    </td>
-                </tr>
-            </tbody>
+                    <button @click="deleteVaccine(item.VaccineID)" class="action-btn">Delete</button>
+                    <button @click="modifyVaccine(item.VaccineID)" class="action-btn">Modify</button>
+                </td>
+            </tr>
+        </tbody>
     </table>
 </template>
 
-<style scoped/>
+<style scoped>
+.tabel {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+.tabel th, .tabel td {
+  border: 1px solid #FFD700;
+  padding: 10px;
+  text-align: left;
+  vertical-align: middle;
+}
+
+.tabel th {
+  background-color: #000000;
+  color: #FFD700;
+}
+.actions {
+    display: flex;
+    gap: 5px; 
+}
+
+.action-btn {
+  padding: 5px 10px;
+  border-radius: 4px;
+  border: none;
+  cursor: pointer;
+  color: #fff;
+  font-weight: bold;
+}
+</style>
+
