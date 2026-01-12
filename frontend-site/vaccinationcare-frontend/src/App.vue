3b7e144cfb73631711f4b8eb0dee94e7d88b50dd
mur3l <tippitauno@gmail.com>
2026-01-12 11:16:55 +0200

    Adding missing | _Commit

--- frontend-site/vaccinationcare-frontend/src/App.vue
+++ frontend-site/vaccinationcare-frontend/src/App.vue
@@ -5,7 +5,7 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
     <router-link to="/vaccines">Vaccines</router-link>
   </nav>
   <router-view />
