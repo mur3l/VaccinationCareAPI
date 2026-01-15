65f119b822a0f352e0a0f4f88ebdcc659af47f3d
JelenaPenkina <122533981+JelenaPenkina@users.noreply.github.com>
2026-01-15 19:34:32 +0200

    Update

--- backend-api/controllers/SessionsController.js
+++ backend-api/controllers/SessionsController.js
@@ -1,26 +1,46 @@
 const { use } = require("../db.js");
 const Utilities = require("./Utilities.js")
 
-
-
-//As an app developer, I want to authenticat a valid existing Session. for any User that authenticates Branch
-exports.reauthenticate =
-async (req.res) => {
-    if(!req.session.ClientID) {
-        return res.status(401).send({error:"Session expired, please log in again."})
-    }
-    var client = await db.clients.findByPk(req.session.ClientID)
-    if(!client)
+exports.newSession =
+async (req,res) => {
+    console.log(req.body)
+    if (req.body.LoginEmail === undefined || !req.body.LoginPassword === undefined)
     {
-    return res.status(401).send({message:"Logged in client not found, please log in again."})
+        var missingparams = "";
+        if(!req.body.LoginEmail)
+        {
+            missingparams += "No email provided.";
+        }
+        if(!req.body.LoginPassword)
+        {
+            missingparams += "No password provided.";
+        }
+        return res.status(400).send({error:"Missing paramteter for logging in"+missingparams})
     }
-    return res.status(200).sed({
-        ClientID : client.ClientID,
-        DisplayName : client.DisplayName,
-        EmailAddress : client.EmailAddress,
-        IsAdmin : client.IsAdmin
+    LoginEmail = req.body.LoginEmail
+    console.log(LoginEmail)
+    var clientToProvideSessionFor = await db.clients.findOne({where: {EmailAddress: LoginEmail}})
+    if(!clientToProvideSessionFor) {
+        return res.status(404).send({error:"Client not found"})
+    }
+    //const newLoginHASH = (await Utilities.gimmePassword(LoginPassword)).toString()
+    var isCorrect = (await Utilities.letMeIn(req.body.LoginPassword, clientToProvideSessionFor.PasswordHASH))
+    if (!isCorrect)
+    {
+        return res.status(401).send({error: "Password mismatch"})
+    }
+    req.session.Client = clientToProvideSessionFor.ClientID
+
+    return res.status(200).send({
+        ClientID : clientToProvideSessionFor.ClientID,
+        DisplayName : clientToProvideSessionFor.DisplayName,
+        EmailAddress : clientToProvideSessionFor.EmailAddress,
+        IsAdmin : clientToProvideSessionFor.IsAdmin
     })
 }
+
+//As an app developer, I want to authenticat a valid existing Session. for any User that authenticates Branch
+
 
 exports.removeSession = 
 async (req,res) => {
