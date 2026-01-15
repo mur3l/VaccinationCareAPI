81400c47928f60cbce50b9f6d7e20ec030db2614
JelenaPenkina <122533981+JelenaPenkina@users.noreply.github.com>
2026-01-15 19:32:10 +0200

    As an app developer, I want to authenticate a valid existing Session, for any Client that authenticates.
    As an app developer, I want to authenticate a valid existing Session, for any Client that authenticates.

--- backend-api/controllers/SessionsController.js
+++ backend-api/controllers/SessionsController.js
@@ -1,7 +1,43 @@
 const { use } = require("../db.js");
 const Utilities = require("./Utilities.js")
 
+exports.newSession =
+async (req,res) => {
+    console.log(req.body)
+    if (req.body.LoginEmail === undefined || !req.body.LoginPassword === undefined)
+    {
+        var missingparams = "";
+        if(!req.body.LoginEmail)
+        {
+            missingparams += "No email provided.";
+        }
+        if(!req.body.LoginPassword)
+        {
+            missingparams += "No password provided.";
+        }
+        return res.status(400).send({error:"Missing paramteter for logging in"+missingparams})
+    }
+    LoginEmail = req.body.LoginEmail
+    console.log(LoginEmail)
+    var clientToProvideSessionFor = await db.clients.findOne({where: {EmailAddress: LoginEmail}})
+    if(!clientToProvideSessionFor) {
+        return res.status(404).send({error:"Client not found"})
+    }
+    //const newLoginHASH = (await Utilities.gimmePassword(LoginPassword)).toString()
+    var isCorrect = (await Utilities.letMeIn(req.body.LoginPassword, clientToProvideSessionFor.PasswordHASH))
+    if (!isCorrect)
+    {
+        return res.status(401).send({error: "Password mismatch"})
+    }
+    req.session.Client = clientToProvideSessionFor.ClientID
 
+    return res.status(200).send({
+        ClientID : clientToProvideSessionFor.ClientID,
+        DisplayName : clientToProvideSessionFor.DisplayName,
+        EmailAddress : clientToProvideSessionFor.EmailAddress,
+        IsAdmin : clientToProvideSessionFor.IsAdmin
+    })
+}
 
 //As an app developer, I want to authenticat a valid existing Session. for any User that authenticates Branch
 exports.reauthenticate =
