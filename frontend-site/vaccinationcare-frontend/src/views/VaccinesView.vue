4be5160a2683225d90b3731bd75d3a586a9d30e6
JelenaPenkina <122533981+JelenaPenkina@users.noreply.github.com>
2026-01-20 13:25:35 +0200

    Update VaccineView
    Parandatud

--- frontend-site/vaccinationcare-frontend/src/views/VaccinesView.vue
+++ frontend-site/vaccinationcare-frontend/src/views/VaccinesView.vue
@@ -33,7 +33,7 @@ const error = ref(null)
 
 onMounted(async () => {
   try {
-    const res = await fetch('http://localhost:3000/vaccinations')
+    const res = await fetch('http://localhost:3000/vaccination')
     if (!res.ok) throw new Error('Failed to fetch')
     vaccines.value = await res.json()
   } catch (e) {
